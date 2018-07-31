a3d5ede42b49b5b4b4487192adbdbbe69461b7e2
yanchao <phperyanchao@126.com>
2018-07-31 20:32:39 +0800

    rm poetry

--- poetry/utils/ald-stat-conf.js
+++
@@ -1,6 +0,0 @@
-exports.app_key = "8d22511588e46538039c4fa0220f1900"; //请在此行填写从阿拉丁后台获取的appkey
-exports.getLocation = false; //默认不获取用户坐标位置
-exports.getUserinfo = true;
-exports.appid = "wx5b6ad08cb2fb7406"; //用于用户登录、微信转发群信息、二维码等微信官方功能
-exports.appsecret = "f35137f74b3bcb8d38c70acf59bbed09";//用于用户登录、微信转发群信息、二维码等微信官方功能
-exports.defaultPath = 'pages/top/top';//小程序的默认首页, 用于分享时path为空时
